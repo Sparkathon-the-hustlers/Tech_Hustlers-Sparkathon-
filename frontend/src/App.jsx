a4ae89b721f6ae845a707067db5e3e14dfb05712
Mithun Dutta <mithundu632@gmail.com>
2025-06-21 21:02:32 +0530

    Initial commit: Set up React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello, React!</h1>
+      <p>This is a clean slate React project.</p>
+    </div>
+  );
+}
+
+export default App;
