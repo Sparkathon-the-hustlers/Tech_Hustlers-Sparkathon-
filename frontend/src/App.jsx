f7b6de487077a11e965494242c4664e8ab168813
Mithun Dutta <mithundu632@gmail.com>
2025-06-21 21:54:10 +0530

    Add initial navbar structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello, React!</h1>
-      <p>This is a clean slate React project.</p>
-    </div>
+    <>
+      <Navbar/>
+      <div className="app">
+        <h1>Hello, React!</h1>
+        <p>This is a clean slate React project.</p>
+      </div>
+    </>
   );
 }
 
