55ddb493e71b09309831b8ed3094075ad3e4d08f
Mithun Dutta <mithundu632@gmail.com>
2025-06-23 11:15:40 +0530

    nav options

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,13 +3,14 @@ import './Navbar.css';
 function Navbar(){
     return(
         <nav className="navbar">
-            <div className="nav-logo">web.logo</div>
+            {/* <div className="nav-logo">web.logo</div> */}
+            <a href="#Home" className='nav-logo'>web.logo</a>
             <input type="search" placeholder="search" className="nav-search"/>
             <ul className='nav-links'>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">Home</a></li>
+                <li><a href="/">Store</a></li>
+                <li><a href="/">Cart</a></li>
+                <li><a href="/">Login</a></li>
             </ul>
         </nav>
     );
